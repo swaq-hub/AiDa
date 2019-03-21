7bd7b9e0b22bad50d48c4b1d604e9d2fffad25af
JoeMayami <36084817+JoeMayami@users.noreply.github.com>
2019-03-21 14:11:44 +0100

    update app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def verify():
 
 @app.route('/', methods=['POST'])
 def webhook():
-    data = request.get_json()
+    data = request
     log("LOG_ " + str(data))
 
     # if data['object']  == 'page':
